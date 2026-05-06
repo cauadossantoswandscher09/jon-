83616e7a1e939863e617471a602546594ed4893f
cauadossantoswandscher09 <caua.wandscher@escola.pr.gov.br>
2026-05-06 12:09:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function cafe() {
+
+        fetch ('dados.json')
+    .then (response => response.json())
+    .then (dadinho =>{
+        document.getElementById('destino').innerHTML =
+            `<div clas="card">
+            <img src=
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+}
